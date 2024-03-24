41188abec290943089a2d94599538556327c28a5
rachid <rachid12br@gmail.com>
2024-03-24 07:40:34 +0100

    Creating Express Server

--- API/server.js
+++ API/server.js
@@ -1,2 +1,10 @@
 require("dotenv").config();
 console.log("the valus is", process.env.NODE_ENV);
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 9000;
+
+app.listen(PORT, () => {
+  console.log("server running on port " + PORT);
+});

77bc8836a028e49ea4793af0f50c652d10b1d526
rachid <rachid12br@gmail.com>
2024-03-24 07:55:38 +0100

    Connect to MongoDB

--- API/server.js
+++ API/server.js
@@ -1,10 +1,19 @@
 require("dotenv").config();
 console.log("the valus is", process.env.NODE_ENV);
-
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+const connectDB = require("./config/dbConn");
 const PORT = process.env.PORT || 9000;
 
-app.listen(PORT, () => {
-  console.log("server running on port " + PORT);
+connectDB();
+
+mongoose.connection.once("open", () => {
+  console.log("connect to mongoDb");
+  app.listen(PORT, () => {
+    console.log("server running on port " + PORT);
+  });
+});
+mongoose.connection.on("error", (err) => {
+  console.log(err);
 });

2f6090c027be3c7a750e4c6273d59f8640bf6972
rachid <rachid12br@gmail.com>
2024-03-25 17:36:22 +0100

    Creating Login endpoint

--- API/controllers/authContoroller.js
+++ API/controllers/authContoroller.js
@@ -4,17 +4,15 @@ const jwt = require("jsonwebtoken");
 const register = async (req, res) => {
   const { first_name, last_name, email, password } = req.body;
   if (!first_name || !last_name || !email || !password) {
-    res.status(400).json({
+    return res.status(400).json({
       message: "All fields are required",
     });
-    return;
   }
   const foundUser = await User.findOne({ email }).exec();
   if (foundUser) {
-    res.status(400).json({
+    return res.status(400).json({
       message: "User already exists",
     });
-    return;
   }
   const hashedPassword = await bcrypt.hash(password, 10);
   const user = await User.create({
@@ -56,7 +54,61 @@ const register = async (req, res) => {
     last_name: user.last_name,
   });
 };
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({
+      message: "All fields are required",
+    });
+  }
+  const foundUser = await User.findOne({ email }).exec();
+  if (!foundUser) {
+    return res.status(400).json({
+      message: "User does not  exists",
+    });
+  }
+
+  const match = await bcrypt.compare(password, foundUser.password);
+
+  if (!match) {
+    return res.status(401).json({
+      message: "Invalid password",
+    });
+  }
+
+  const accessTokent = jwt.sign(
+    {
+      UserInfo: {
+        id: foundUser._id,
+      },
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: "15m" }
+  );
+  const refreshToken = jwt.sign(
+    {
+      UserInfo: {
+        id: foundUser._id,
+      },
+    },
+    process.env.REFRECH_TOKEN_SECRET,
+    { expiresIn: "7d" }
+  );
+
+  res.cookie("jwt", refreshToken, {
+    httpOnly: true, //accessible only by hhtp
+    secure: true, //https
+    sameSite: "None", //cross-site cookie
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+
+  res.json({
+    accessTokent,
+    email: foundUser.email,
+  });
+};
 
 module.exports = {
   register,
+  login,
 };
--- API/routes/authRoutes.js
+++ API/routes/authRoutes.js
@@ -2,5 +2,6 @@ const express = require("express");
 const router = express.Router();
 const authContoroller = require("../controllers/authContoroller");
 router.route("/register").post(authContoroller.register);
+router.route("/login").post(authContoroller.login);
 
 module.exports = router;

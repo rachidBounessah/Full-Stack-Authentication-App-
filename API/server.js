6ce95f365409a76ecf2bd5ec436cc6f068576dbb
rachid <rachid12br@gmail.com>
2024-03-25 14:14:48 +0100

    Adding Error Handling

--- API/server.js
+++ API/server.js
@@ -18,6 +18,17 @@ app.use(express.json());
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/", require("./routes/root"));
 
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "VIEWS", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 not found" });
+  } else {
+    res.type("txt").send("404 not found");
+  }
+});
+
 mongoose.connection.once("open", () => {
   console.log("connect to mongoDb");
   app.listen(PORT, () => {
